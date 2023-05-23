26b6d4474d0062d611ba044df8d9e4384cf4b510
John Maybee <john.d.maybee@gmail.com>
2023-05-23 18:33:20 -0500

    refactor: created a pokedex component and practiced Composition APIs and learned the Suspense element

--- vue practice/src/App.vue
+++ vue practice/src/App.vue
@@ -1,30 +1,55 @@
 <script>
-// https://pokeapi.co/api/v2/pokemon?limit=151
+import Pokedex from './components/pokedex.vue';
 
 export default {
-  data: () => ({
-    pokedex: [1, 2, 3],
-  }),
-  methods: {
-    async fetchPokemon() {
-      this.pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-      .then(response => response.json());
-    }
-  },
-  // beforeCreate() {
-  //   console.log('beforeCreate')
-  //   console.log(this.pokedex)
-  // },
-  created() {
-    this.fetchPokemon();
+  components: {
+    Pokedex,
   },
 }
+// // https://pokeapi.co/api/v2/pokemon?limit=151
+
+// export default {
+//   async setup() {
+//     const regionName = 'Kanto';
+    
+//     const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+//       .then(response => response.json());
+//     return {
+//       pokedex,
+//       regionName,
+//     };
+//   },
+//   data: () => ({
+//     pokedex: [1, 2, 3],
+//   }),
+//   // methods: {
+//   //   async fetchPokemon() {
+//   //     this.pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+//   //     .then(response => response.json());
+//   //   }
+//   // },
+//   // beforeCreate() {
+//   //   console.log('beforeCreate')
+//   //   console.log(this.pokedex)
+//   // },
+//   created() {
+//     // this.fetchPokemon();
+
+//     console.log(this.regionName);
+//     console.log(this.pokedex);
+//   },
+// }
 </script>
 
 <template>
   <h1>New App</h1>
-  <pre>{{ pokedex }}</pre>
-  <button @click="fetchPokemon">Fetch Pokemon</button>
+  <Suspense>
+    <Pokedex />
+
+    <template v-slot:fallback> Loading Pokedex data... </template>
+  </Suspense>
+  <!-- <pre>{{ pokedex }}</pre>
+  <button @click="fetchPokemon">Fetch Pokemon</button> -->
 </template>
 
 <style></style>
---
+++ vue practice/src/components/pokedex.vue
@@ -0,0 +1,42 @@
+<script>
+// https://pokeapi.co/api/v2/pokemon?limit=151
+
+export default {
+  async setup() {
+    const regionName = 'Kanto';
+    
+    const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => response.json());
+    return {
+      pokedex,
+      regionName,
+    };
+  },
+  data: () => ({
+    pokedex: [1, 2, 3],
+  }),
+  // methods: {
+  //   async fetchPokemon() {
+  //     this.pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  //     .then(response => response.json());
+  //   }
+  // },
+  // beforeCreate() {
+  //   console.log('beforeCreate')
+  //   console.log(this.pokedex)
+  // },
+  created() {
+    // this.fetchPokemon();
+
+    console.log(this.regionName);
+    console.log(this.pokedex);
+  },
+}
+</script>
+
+<template>
+
+        <pre>{{ pokedex }}</pre>
+
+    
+</template>
